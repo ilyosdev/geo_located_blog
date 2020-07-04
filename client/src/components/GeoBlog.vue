708c01a57f2ee3f7578b8acd8303dd88987c3b82
Aleksey Belchenkov <u608110@gmail.com>
2020-07-04 11:03:02 +0300

    Routing

---
+++ client/src/components/GeoBlog.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="geo-blog">
+    <!-- More to come -->
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "GeoBlog"
+    }
+</script>
+
+<style scoped>
+
+</style>
