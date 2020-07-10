fbb2a5d3ba3316b346de1468c9c1766ad1a9f051
Aleksey Belchenkov <u608110@gmail.com>
2020-07-10 15:36:35 +0300

    Login Google button

--- server/src/config.js
+++ server/src/config.js
@@ -2,8 +2,10 @@ export const PORT = process.env.PORT || 3000
 export const SECRET = process.env.SECRET || 'TR7_9cDZ5Re-@lT3Z1|58F'
 export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:8080'
 export const PUBLIC_URL = process.env.PUBLIC_URL || 'http://localhost:3000'
-export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
-export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+//export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+//export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+export const GOOGLE_CLIENT_ID = '448467644899-eresjpg25as9o3c2o9eih42durqbdul2.apps.googleusercontent.com'
+export const GOOGLE_CLIENT_SECRET = 'YB1_OL_053NQCQCsLvKPNTFp'
 export const DB_PATH = process.env.DB_PATH || './db'
 export const COMPACTION_INTERVAL = process.env.COMPACTION_INTERVAL || 12 * 60 * 60 * 1000
 
