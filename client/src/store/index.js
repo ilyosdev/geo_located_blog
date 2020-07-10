fea363c733a374bc02141fdb0e56116ce44174e3
Aleksey Belchenkov <u608110@gmail.com>
2020-07-10 15:44:10 +0300

    Login & Logout - Store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { $fetch } from "../plugins/fetch";
 
 Vue.use(Vuex);
 
@@ -20,17 +21,38 @@ const store = new Vuex.Store({
     },
   },
   actions: {
-    login({ commit }) {
-      const userData = {
-        profile: {
-          displayName: 'Mr Cat',
-        },
-      };
+    async login({ commit }) {
+      try {
+        const user = await $fetch('user');
+        commit('user', user);
 
-      commit('user', userData);
+        if (user) {
+          // Redirect to the wanted route if any or else to home
+          router.replace(router.currentRoute.params.wantedRoute || { name: 'home' });
+        }
+
+      } catch (e) {
+        console.error(e);
+      }
+
     },
     logout({ commit }) {
       commit('user', null);
+
+      $fetch('logout');
+
+      // If the route is private
+      // We go to the login screen
+      if (router.currentRoute.matched.some(r => r.meta.private)) {
+        router.replace(
+          {
+            name: 'login',
+            params: {
+              wantedRoute: router.currentRoute.fullPath,
+            }
+          }
+        );
+      }
     },
   }
 });
